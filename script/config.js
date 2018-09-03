c9c76f7ac3e02b46b620b571b4417f640d48a768
babyron64 <babyron64@gmail.com>
2018-09-03 19:26:58 +0900

    Change: global app configuration

--- script/config.js
+++ script/config.js
@@ -3,6 +3,10 @@ function initConfig() {
     var IsiPod = navigator.userAgent.indexOf("iPod") != -1;
     var IsiPad= navigator.userAgent.indexOf("iPad") != -1;
 
+    app_info.screen.width = app_info.gl.canvas.width;
+    app_info.screen.height = app_info.gl.canvas.height;
+    app_info.screen.fov = degToRad(45);
+
     debugOnWindow("iPhone: "+IsiPhone+"\n"+
                   "iPod:"+IsiPod+"\n"+
                   "iPad:"+IsiPad, 3);
--- script/main.js
+++ script/main.js
@@ -1,9 +1,65 @@
+/*
+ * dependency:
+ *     planet.js
+ *     setting.js
+ *     ui.js
+ *     utils.js
+ *     frame_manager.js
+ *     location.js
+ *     config.js
+ *     webglconf.js
+ *     astrocalc.js
+ */
+
 //
 // global variables
 //
-var plContainer;
-var manager;
+var app_info = {
+    location: {
+        orientation: {
+            get: null,
+            onchange: null,
+        },
+        position: {
+            get: null,
+            onchange: null,
+        }
+    },
+    gl: {
+        canvas: null,
+        context: null,
+        shaders: {},
+        buffers: {},
+        renders: {}, // see Render class in webglconf.js
+        pool: {}, // see BufferPool class in webglconf.js
+    },
+    screen: {
+        width: null,
+        height: null,
+        fov: null, // radian
+        near: 0.1,
 
+        // radian
+        alpha: null, 
+        beta: null,
+        gamma: null,
+
+        // radian
+        longitude: null,
+        latitude: null,
+    },
+    setting: null, // see setting.js
+    frame: {
+        manager: null,
+    },
+    container: null,
+};
+
+//
+// entry point
+//
+
+// var dummy = async function() {
 window.onload = async function() {
     //
     // Initialization
@@ -22,47 +78,34 @@ window.onload = async function() {
         e.preventDefault();
     });
 
-    const screen = document.querySelector("#plScreen");
-    const gl = screen.getContext("webgl");
-    if (gl === null) {
+    app_info.container = new DIContainer();
+    app_info.frame.manager = new FrameManager();
+
+    app_info.gl.canvas = document.querySelector("#plScreen");
+    app_info.gl.canvas.width = window.innerWidth;
+    app_info.gl.canvas.height = window.innerHeight;
+    app_info.gl.context = app_info.gl.canvas.getContext("webgl");
+    if (app_info.gl.context === null) {
       throw new Error("Unable to initialize WebGL. Your browser or machine may not support it.");
     }
 
-    var pl = new Planetarium(gl);
-    pl.changeOrientation = debugOrientation;
-    pl.changePosition = debugPosition;
+    app_info.location.orientation.onchange = updateOrientation;
+    app_info.location.position.onchange = updatePosition;
 
-    plContainer = new DIContainer();
-    plContainer.add("pl", pl);
-
-    manager = new FrameManager();
-
-    initConfig(pl);
-    initLocation(pl);
     initUI();
 
     enterProgress();
 
+    initConfig();
+
+    const manager = app_info.frame.manager;
+    // main pane
     const main = document.querySelector("#main_pane");
     const main_frame = manager.createFrame(main);
     await manager.register(main_frame, "main");
-
+    // setting pane
     const setting = await loadSettingPane();
     const setting_frame = manager.createFrame(setting);
-    const _setting_enter = setting_frame.enter;
-    // change style when entered
-    // const setting_style = setting.querySelector("link");
-    // setting_frame.enter = async function (element) {
-    //     if (!element.contains(setting_style))
-    //         element.appendChild(setting_style);
-    //     return await _setting_enter(element);
-    // };
-    // const _setting_exit = setting_frame.exit;
-    // setting_frame.exit = async function (element) {
-    //     if (element.contains(setting_style))
-    //         setting_style.remove();
-    //     return await _setting_exit(element);
-    // };
     setting_frame.onenable = settingPaneOnEnable;
     setting_frame.ondisable = settingPaneOnDisable;
     await manager.register(setting_frame, "setting");
@@ -70,225 +113,62 @@ window.onload = async function() {
     FrameManager.trans.slideFromBottom(manager, "main", "setting");
     FrameManager.trans.buttonClick(manager, "setting", "main", "#ok_button");
 
-    var shader_confs = [
-        { tag: "vshader", url: "/shader/vshader.vert", type: gl.VERTEX_SHADER },
-        { tag: "fshader", url: "/shader/fshader.frag", type: gl.FRAGMENT_SHADER }
-    ];
-    await initScreen(pl, shader_confs);
-    await plLoadData(pl);
+    await initLocation();
+    await initUtils();
+    await initScreen();
+
+    await initAstroCalc();
+
+    startPlanet();
 
     manager.show("main");
+    // manager.show("setting");
 
     exitProgress();
-
-    plExecute(pl);
 }
 
-async function plLoadData(pl) {
-    setShaders(pl);
-    const objectData = await loadFileJSON("/object/cube.json");
-    plContainer.add("buffers", initBuffers(pl.screen, objectData));
-    plContainer.add("texture", await loadTextureVideo(pl.screen, objectData.video_url));
+var diff_time = 0;
+async function loop() {
+    const time = new Date(Date.now()+diff_time);
+    // const time = getJSTTime(2018, 9, 2, 22);
+    const animation_rate = 0;
+    const pos = new Coord2(app_info.screen.longitude, app_info.screen.latitude, "NOW", time);
+
+    app_info.setting.state.time_ctl.time.set(time);
+    app_info.setting.state.time_ctl.animation_rate.set(animation_rate);
+
+    clearScreen();
+
+    await hipStarProject(star_coords.filter(e => notEmpty(e.Vmag) && e.Vmag<6), time, pos);
+
+    const planet_names = ["Mercury", "Venus", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto"];
+    await planetsProject(planet_names, time, pos);
+
+    await sunProject(time, pos);
+
+    diff_time = diff_time+animation_rate;
 }
 
-function plExecute(pl) {
-    // Used for rotation animation.
-    var cubeRotation = 0;
-    function render(now) {
-        cubeRotation = now / 1000;
-        drawScene(pl.screen, pl.glInfo, plContainer.get("buffers"), plContainer.get("texture"), cubeRotation);
-        requestAnimationFrame(render);
+function startPlanet() {
+    var frame_counter = 0;
+    async function frame_loop () {
+        frame_counter = frame_counter + 1;
+        await loop();
+        requestAnimationFrame(frame_loop);
     }
-    requestAnimationFrame(render);
-}
+    frame_loop();
 
-function setShaders(pl) {
-    plAttachShaders(pl, ["vshader", "fshader"]);
-
-    // Corresponding to shaders in use.
-    plRegisterAttrib(pl, "VertexPosition");
-    plRegisterAttrib(pl, "VertexColor");
-    plRegisterAttrib(pl, "VertexNormal");
-    plRegisterAttrib(pl, "TextureCoord");
-
-    plRegisterUniform(pl, "ProjectionMatrix");
-    plRegisterUniform(pl, "ModelViewMatrix");
-    plRegisterUniform(pl, "NormalMatrix");
-    plRegisterUniform(pl, "Sampler");
-}
-
-function initBuffers(gl, objectData) {
-    const positionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER,
-                  new Float32Array(objectData.position),
-                  gl.STATIC_DRAW);
-    
-    const colorBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER,
-                  new Float32Array(objectData.color),
-                  gl.STATIC_DRAW);
-
-    const indexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER,
-                  new Uint16Array(objectData.index),
-                  gl.STATIC_DRAW);
-
-    const textureBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, textureBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER,
-                  new Float32Array(objectData.texture),
-                  gl.STATIC_DRAW);
-
-    const normalBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER,
-                  new Float32Array(objectData.normal),
-                  gl.STATIC_DRAW);
-
-    return {
-        position: positionBuffer,
-        color: colorBuffer,
-        index: indexBuffer,
-        texture: textureBuffer,
-        normal: normalBuffer,
-    };
-}
-
-function drawScene(gl, programInfo, buffers, texture, cubeRotation) {
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
-    gl.clearDepth(1.0);                 // Clear everything
-    gl.enable(gl.DEPTH_TEST);           // Enable depth testing
-    gl.depthFunc(gl.LEQUAL);            // Near things obscure far things
-  
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-  
-    const fieldOfView = 45 * Math.PI / 180;   // in radians
-    const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
-    const zNear = 0.1;
-    const zFar = 100.0;
-    const projectionMatrix = mat4.create();
-    mat4.perspective(projectionMatrix,
-                     fieldOfView,
-                     aspect,
-                     zNear,
-                     zFar);
-  
-    const modelViewMatrix = mat4.create();
-    mat4.translate(modelViewMatrix,     // destination matrix
-                   modelViewMatrix,     // matrix to translate
-                   [-0.0, 0.0, -6.0]);  // amount to translate
-    mat4.rotate(modelViewMatrix,  // destination matrix
-                modelViewMatrix,  // matrix to rotate
-                cubeRotation,   // amount to rotate in radians
-                [0, 0, 1]);       // axis to rotate around
-    mat4.rotate(modelViewMatrix,  // destination matrix
-                modelViewMatrix,  // matrix to rotate
-                cubeRotation*0.7,   // amount to rotate in radians
-                [0, 1, 0]);       // axis to rotate around
-    
-    const normalMatrix = mat4.create();
-    mat4.invert(normalMatrix, modelViewMatrix);
-    mat4.transpose(normalMatrix, normalMatrix);
-
-    {
-        const numComponents = 3;  // pull out 2 values per iteration
-        const type = gl.FLOAT;    // the data in the buffer is 32bit floats
-        const normalize = false;  // don't normalize
-        const stride = 0;         // how many bytes to get from one set of values to the next
-        const offset = 0;         // how many bytes inside the buffer to start from
-        gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
-        gl.vertexAttribPointer(
-            programInfo.attribLocations.VertexPosition,
-            numComponents,
-            type,
-            normalize,
-            stride,
-            offset);
-        gl.enableVertexAttribArray(
-            programInfo.attribLocations.VertexPosition);
+    function calcFPS() {
+        app_info.setting.state.time_ctl.fps.set(frame_counter);
+        frame_counter = 0;
+        setTimeout(calcFPS, 1000);
     }
-    {
-        const numComponents = 4;  // pull out 2 values per iteration
-        const type = gl.FLOAT;    // the data in the buffer is 32bit floats
-        const normalize = false;  // don't normalize
-        const stride = 0;         // how many bytes to get from one set of values to the next
-        const offset = 0;         // how many bytes inside the buffer to start from
-        gl.bindBuffer(gl.ARRAY_BUFFER, buffers.color);
-        gl.vertexAttribPointer(
-            programInfo.attribLocations.VertexColor,
-            numComponents,
-            type,
-            normalize,
-            stride,
-            offset);
-        gl.enableVertexAttribArray(
-            programInfo.attribLocations.VertexColor);
-    }
-    {
-        const numComponents = 2; // every coordinate composed of 2 values
-        const type = gl.FLOAT; // the data in the buffer is 32 bit float
-        const normalize = false; // don't normalize
-        const stride = 0; // how many bytes to get from one set to the next
-        const offset = 0; // how many bytes inside the buffer to start from
-        gl.bindBuffer(gl.ARRAY_BUFFER, buffers.texture);
-        gl.vertexAttribPointer(
-            programInfo.attribLocations.TextureCoord,
-            numComponents,
-            type,
-            normalize,
-            stride,
-            offset);
-        gl.enableVertexAttribArray(programInfo.attribLocations.TextureCoord);
-    }
-    {
-        const numComponents = 3;
-        const type = gl.FLOAT;
-        const normalize = false;
-        const stride = 0;
-        const offset = 0;
-        gl.bindBuffer(gl.ARRAY_BUFFER, buffers.normal);
-        gl.vertexAttribPointer(
-            programInfo.attribLocations.VertexNormal,
-            numComponents,
-            type,
-            normalize,
-            stride,
-            offset);
-        gl.enableVertexAttribArray(programInfo.attribLocations.VertexNormal);
-    }
-
-    gl.useProgram(programInfo.program);
-
-    gl.uniformMatrix4fv(
-        programInfo.uniformLocations.ProjectionMatrix,
-        false,
-        projectionMatrix);
-    gl.uniformMatrix4fv(
-        programInfo.uniformLocations.ModelViewMatrix,
-        false,
-        modelViewMatrix);
-    gl.uniformMatrix4fv(
-        programInfo.uniformLocations.NormalMatrix,
-        false,
-        normalMatrix);
-  
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.index);
-
-    gl.activeTexture(gl.TEXTURE0);
-    gl.bindTexture(gl.TEXTURE_2D, texture);
-    gl.uniform1i(programInfo.uniformLocations.uSampler, 0);
-
-    {
-        const vertexCount = 36;
-        const type = gl.UNSIGNED_SHORT;
-        const offset = 0;
-        gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
-    }
+    calcFPS();
 }
 
 function debugOnWindow(msg, num) {
-    document.getElementById("debug"+num.toString()).innerHTML = msg;
+    const debug = document.getElementById("debug"+num.toString());
+    if (debug) {
+        debug.innerHTML = msg;
+    }
 }
